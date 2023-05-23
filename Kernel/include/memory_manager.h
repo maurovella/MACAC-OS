130dee6c513b7c2d5a42f8c507cb79ea9daa45e6
santiagohirsch <shirsch@itba.edu.ar>
2023-05-23 20:34:06 -0300

    malloc implementation (not tested)

--- Kernel/include/memory_manager.h
+++ Kernel/include/memory_manager.h
@@ -10,6 +10,8 @@
 #define TRUE 1
 #define FALSE 0
 #define SIZE(size) ((size) & ~0x1) // masks the last bit of size, gets just the size
+#define MIN_SIZE (sizeof(header_info) + 2) // TODO: check if 2 is correct or 1
+#define MAX_SIZE (HEAP_SIZE - sizeof(header_info))
 
 typedef struct memory_info {
     uint64_t allocated_bytes;   
@@ -26,6 +28,12 @@ void memory_init();
 
 void memory_free(void * ptr);
 
-void freeBlock(header_info * ptr);
+void free_block(header_info * ptr);
+
+void * memory_alloc(uint64_t _size);
+
+header_info * find_free_block(uint64_t _size);
+
+void add_block(header_info * ptr, uint64_t _size);
 
 #endif
