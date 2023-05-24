89228c67f09201c23a9307e176dfb696bb84fa5d
maurov <mvella@itba.edu.ar>
2023-05-24 17:43:53 -0300

    feat(idle): add idle process

--- Kernel/include/scheduler.h
+++ Kernel/include/scheduler.h
@@ -1,5 +1,6 @@
 #ifndef SCHEDULER_H
 #define SCHEDULER_H
 
+void idle_process();
 
 #endif // SCHEDULER_H
--- Kernel/process_manager/scheduler.c
+++ Kernel/process_manager/scheduler.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include "scheduler.h"
+#include "interrupts.h"
 
 #define FLAGS            0x202				
 #define STACK_SEGMENT    0x0				
@@ -7,24 +8,29 @@
 #define MAX_PROCESSES    20
 
 typedef struct process {
-    uint8_t pid;                  // ID del proceso
-    uint8_t priority;             // Prioridad del proceso
-    void (*function)(void *);     // Puntero a la función del proceso
-    void **params;                // Parámetros del proceso (arreglo de cadenas)
-    void *stack_start;            // Inicio del stack del proceso
-    void *stack_end;              // Final del stack del proceso
-    void *stack_pointer;          // Puntero al stack actual del proceso
-    uint8_t state;                // Estado del proceso (BLOCK, READY, PAUSED)
-    uint32_t ticks;               // Ticks asignados por el scheduler
-    uint8_t immortal;             // Indica si el proceso es inmortal
-    uint8_t *input;             // Descriptor/identificador entrada asociado al proceso
-    uint8_t *output;            // Descriptor/identificador salida asociado al proceso
+    uint8_t pid;                    // ID del proceso
+    uint8_t priority;               // Prioridad del proceso
+    void (*function)(void *);       // Puntero a la función del proceso
+    void **params;                  // Parámetros del proceso (arreglo de cadenas)
+    
+    void *stack_start;              // Inicio del stack del proceso
+    void *stack_end;                // Final del stack del proceso
+    void *stack_pointer;            // Puntero al stack actual del proceso
+    
+    uint8_t state;                  // Estado del proceso (BLOCK, READY, PAUSED)
+    uint32_t ticks;                 // Ticks asignados por el scheduler
+    uint8_t immortal;               // Indica si el proceso es inmortal
+    
+    uint8_t *input;                 // Descriptor/identificador entrada asociado al proceso
+    uint8_t *output;                // Descriptor/identificador salida asociado al proceso
 } process;
 
+process process_list[MAX_PROCESSES];    // Lista de procesos
 
-process process_list[MAX_PROCESSES]; // Lista de procesos
+static uint8_t current_pid = 1;         // Indice del proceso actual
 
-
-
-
-
+void idle_process() {
+    while (1) {
+        _hlt();
+    }
+}
