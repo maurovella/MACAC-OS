4c1cfff5d97de4e4a07f6ec78119ee84d5770974
shirsch <shirsch@itba.edu.ar>
2023-05-25 17:16:00 -0300

    Advances on scheduler (create_process)

--- Kernel/include/scheduler.h
+++ Kernel/include/scheduler.h
@@ -1,15 +1,34 @@
 #ifndef SCHEDULER_H
 #define SCHEDULER_H
 
-#define FLAGS            0x202				
-#define STACK_SEGMENT    0x0				
-#define CODE_SEGMENT     0x8
-#define MAX_PROCESSES    20
-#define NO_PROCESS_FOUND -1
-#define TRUE             1
-#define BLOCK            0
-#define READY            1
-#define PAUSED           2
+#define FLAGS                0x202				
+#define STACK_SEGMENT        0x0				
+#define CODE_SEGMENT         0x8
+#define MAX_PROCESSES        20
+#define NO_PROCESS_FOUND     -1
+#define TRUE                 1
+#define DEAD                 0
+#define BLOCK                1
+#define READY                2
+#define PAUSED               3
+#define NO_SPACE_FOR_PROCESS -2
+#define PROCESSES_LIMIT_REACHED    -3
+#define STACK_SIZE           4096 // 4KB
+
+// Stack structure 
+#define STACK_POINT_OF_ENTRY (21*8)   	/*  	|	RAX, RBX  |		*/
+										/*  	|	RCX, etc  |		*/   
+#define RDI_POS   (12*8)				/*		---------------		*/ 
+#define IP_POS    (6*8)					/*  	|	  RIP	  |		*/				
+#define CS_POS 	  (5*8)					/*  	|	  CS	  |		*/
+#define FLAGS_POS (4*8)					/*  	|	  RFLAGS  |		*/
+#define SP_POS 	  (3*8)					/*  	|	  RSP	  |		*/
+#define SS_POS	  (2*8)					/*  	|	  SS	  |		*/
+#define RET_POS   8 					/*  	|	  RET	  |		*/
+#define ALINGMENT 8		 				/*   	 -------------		*/
+
+#define STACK_POSITION(stack_start, pos) (uint64_t *) ((stack_start) - (pos))
+#define CALCULATE_TICKS(priority) (priority) * 2
 
 void idle_process();
 
--- Kernel/process_manager/scheduler.c
+++ Kernel/process_manager/scheduler.c
@@ -1,25 +1,24 @@
 #include <stdint.h>
 #include "scheduler.h"
 #include "interrupts.h"
-
-
+#include "memory_manager.h"
 
 typedef struct process {
-    uint8_t pid;                    // ID del proceso
-    uint8_t priority;               // Prioridad del proceso
-    void (*function)(void *);       // Puntero a la función del proceso
-    void **params;                  // Parámetros del proceso (arreglo de cadenas)
+    uint8_t pid;                    // process ID
+    uint8_t priority;               // priority of the process (1-5)
+
+    char **params;                  // parameters of the process
     
-    void *stack_start;              // Inicio del stack del proceso
-    void *stack_end;                // Final del stack del proceso
-    void *stack_pointer;            // Puntero al stack actual del proceso
+    void *stack_start;              // start of the stack of the process
+    void *stack_end;                // end of the stack of the process
+    void *stack_pointer;            // pointer to the stack of the process
     
-    uint8_t state;                  // Estado del proceso (BLOCK, READY, PAUSED)
-    uint32_t ticks;                 // Ticks asignados por el scheduler
-    uint8_t immortal;               // Indica si el proceso es inmortal
+    uint8_t state;                  // state of the process 
+    uint32_t ticks;                 // asigned ticks to the process
+    uint8_t immortal;               // indicates if the process is immortal (1) or not (0)
     
-    uint8_t *input;                 // Descriptor/identificador entrada asociado al proceso
-    uint8_t *output;                // Descriptor/identificador salida asociado al proceso
+    uint8_t input;                 // direction of the input
+    uint8_t output;                // direction of the output
 } process;
 
 process process_list[MAX_PROCESSES];    // Lista de procesos
@@ -79,3 +78,60 @@ uint8_t block_or_unblock(uint8_t pid) {
     process_list[idx].state = (process_list[idx].state == BLOCK) ? READY : BLOCK;
     return TRUE;
 }
+
+uint64_t build_stack(uint64_t entry_point, uint64_t stack_end, char **params) {
+    uint64_t stack_start = stack_end + STACK_SIZE;
+    stack_start -= stack_start % ALINGMENT;
+
+    *(STACK_POSITION(stack_start, RDI_POS)) = (uint64_t) params;
+
+    // TODO: change magic numbers
+    for(int i = 7; i < 21; i++){
+		if(i != 12)
+			*(STACK_POSITION(stack_start, i * 8)) = 0;
+	}
+
+    *(STACK_POSITION(stack_start, IP_POS)) = entry_point;
+    *(STACK_POSITION(stack_start, CS_POS)) = CODE_SEGMENT;
+    *(STACK_POSITION(stack_start, FLAGS_POS)) = FLAGS;
+    *(STACK_POSITION(stack_start, SP_POS)) = stack_start - RET_POS;
+    *(STACK_POSITION(stack_start, SS_POS)) = STACK_SEGMENT;
+    *(STACK_POSITION(stack_start, RET_POS)) = 0;
+    return stack_start;
+    
+}
+
+uint8_t create_process(char **params, uint8_t priority, uint8_t input, uint8_t output, uint8_t immortal, uint64_t entry_point) {
+    // check if there is space for a new process
+    if(dim == MAX_PROCESSES) {
+        return PROCESSES_LIMIT_REACHED;
+    }
+    // TODO "remember it´s a premature Operative System, please don´t abuse"
+
+    int pos;
+	for(pos = 0; process_list[pos].state != DEAD; pos++);	// find a free space
+
+    // Build the stack process
+    uint8_t *stack_end = memory_alloc(STACK_SIZE);
+    
+    if (stack_end == NULL) {
+        return NO_SPACE_FOR_PROCESS;
+    }
+    
+    uint8_t *stack_start = (uint8_t *) build_stack(entry_point, stack_end, params);
+
+    process_list[pos].pid = pid_value++;
+    process_list[pos].priority = priority;
+    process_list[pos].params = params;
+    process_list[pos].stack_start = stack_start;
+    process_list[pos].stack_end = stack_end;
+    process_list[pos].stack_pointer = stack_start;
+    process_list[pos].state = READY;
+    process_list[pos].ticks = CALCULATE_TICKS(priority);
+    process_list[pos].immortal = immortal;
+    process_list[pos].input = input;
+    process_list[pos].output = output;
+    
+    dim++;
+}
+
