88175858f596b3044f79f4df5b2e571530bbbc8d
maurov <mvella@itba.edu.ar>
2023-05-31 12:41:35 -0300

    fix(run): bugfix

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if [[ "$1" == "gdb" ]]; then
+if [[ "$1" == "gdb" ]];
+then
      qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512 -d int -audiodev coreaudio,id=audio0 -machine pcspk-audiodev=audio0
-else if [[ "$1" == "-na" ]]; then #no audio
+elif [[ "$1" == "-na" ]]; #no audio
+then
      qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512
 else
      qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev coreaudio,id=audio0 -machine pcspk-audiodev=audio0
