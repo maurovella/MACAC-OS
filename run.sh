7f0206d789fcfb750d94922b15bc1f65a204ba49
maurov <mvella@itba.edu.ar>
2023-05-31 12:39:40 -0300

    feat(run): now supports no audio flag

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 
 if [[ "$1" == "gdb" ]]; then
      qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512 -d int -audiodev coreaudio,id=audio0 -machine pcspk-audiodev=audio0
+else if [[ "$1" == "-na" ]]; then #no audio
+     qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512
 else
      qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev coreaudio,id=audio0 -machine pcspk-audiodev=audio0
 fi
