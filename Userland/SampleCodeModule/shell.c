f40280878cccf1cfb20faa02d0c68094976a3152
brunibaumgart <bbaumgart@itba.edu.ar>
2023-06-03 23:44:29 -0300

    Commit monito fachero

--- Userland/SampleCodeModule/shell.c
+++ Userland/SampleCodeModule/shell.c
@@ -1,4 +1,6 @@
 #include <shell.h>
+#include <string_util.h>
+#include <stddef.h>
 
 #define NO_PARMAS_ERROR_MSG "Try without parameters\n"
 #define ONE_PARAM_ERROR_MSG "Try with one parameter\n"
@@ -23,7 +25,7 @@ static program_info_CDT programs[] = {
     {.name = "invalidopcode",   .function_ptr = &invalid_op_code,       .min_args = 0,  .max_args = 0, 	.pipeable = 0 , .params_error_msg = NO_PARMAS_ERROR_MSG , .base_priority = 5},
     {.name = "dividebyzero",    .function_ptr = &divide_by_zero,        .min_args = 0,  .max_args = 0, 	.pipeable = 0 , .params_error_msg = NO_PARMAS_ERROR_MSG , .base_priority = 5},
     {.name = "inforeg", 		.function_ptr = &inforeg, 			    .min_args = 0, 	.max_args = 0, 	.pipeable = 0 , .params_error_msg = NO_PARMAS_ERROR_MSG , .base_priority = 5},
-    {.name = "printmem", 		.function_ptr = &print_mem, 		    .min_args = 2, 	.max_args = 2, 	.pipeable = 0 , .params_error_msg = ONE_PARAM_ERROR_MSG , .base_priority = 5},
+    {.name = "printmem", 		.function_ptr = &print_mem, 		    .min_args = 1, 	.max_args = 1, 	.pipeable = 0 , .params_error_msg = ONE_PARAM_ERROR_MSG , .base_priority = 5},
     {.name = "time", 			.function_ptr = &time, 				    .min_args = 0, 	.max_args = 0, 	.pipeable = 0 , .params_error_msg = NO_PARMAS_ERROR_MSG , .base_priority = 5},
     {.name = "changefontsize",  .function_ptr = &change_font_size,  	.min_args = 1, 	.max_args = 1, 	.pipeable = 0 , .params_error_msg = ONE_PARAM_ERROR_MSG , .base_priority = 5},
     {.name = "tron", 			.function_ptr = &tron, 					.min_args = 0, 	.max_args = 0, 	.pipeable = 0 , .params_error_msg = NO_PARMAS_ERROR_MSG , .base_priority = 0},
@@ -41,6 +43,72 @@ static uint8_t validate_size(uint64_t size, program_info_ADT program){
 	return 1;
 }
 
+void * mem_cpy(void * destination, const void * source, uint64_t length)
+{
+	/*
+	* mem_cpy does not support overlapping buffers, so always do it
+	* forwards. (Don't change this without adjusting memmove.)
+	*
+	* For speedy copying, optimize the common case where both pointers
+	* and the length are word-aligned, and copy word-at-a-time instead
+	* of byte-at-a-time. Otherwise, copy by bytes.
+	*
+	* The alignment logic below should be portable. We rely on
+	* the compiler to be reasonably intelligent about optimizing
+	* the divides and modulos out. Fortunately, it is.
+	*/
+	uint64_t i;
+
+	if ((uint64_t)destination % sizeof(uint32_t) == 0 &&
+		(uint64_t)source % sizeof(uint32_t) == 0 &&
+		length % sizeof(uint32_t) == 0)
+	{
+		uint32_t *d = (uint32_t *) destination;
+		const uint32_t *s = (const uint32_t *)source;
+
+		for (i = 0; i < length / sizeof(uint32_t); i++)
+			d[i] = s[i];
+	}
+	else
+	{
+		uint8_t * d = (uint8_t*)destination;
+		const uint8_t * s = (const uint8_t*)source;
+
+		for (i = 0; i < length; i++)
+			d[i] = s[i];
+	}
+
+	return destination;
+}
+
+static char ** prepare_parameters(char parameters[MAX_PARAMETERS][LENGTH_PARAMETERS], uint64_t size, char* name){
+	void * params_alloc = (void *) sys_malloc((size + 2 ) * sizeof(char *));
+	if(params_alloc == NULL){
+		do_print_color("Error allocating memory for parameters\n", RED);
+		return;
+	}
+	char ** params = (char **) params_alloc;
+
+	void * param_alloc;
+	int param_size;
+
+	for(int i = 0; i < size; i++){
+		param_size = _str_len(parameters[i]) + 1;
+		param_alloc = sys_malloc(param_size);
+		if(param_alloc == NULL){
+			do_print_color("Error allocating memory for parameters\n", RED);
+			return;
+		}
+
+		char * param = (char *) param_alloc;
+		mem_cpy(param, parameters[i], param_size);
+		params[i] = param;
+	}
+	params[size++] = name;
+	params[size] = NULL;
+	return params;
+}
+
 void shell() {
     while(1){
 		printf("$>");
@@ -54,7 +122,8 @@ void shell() {
 		int command_idx = find_idx_command(command);
 		if(command_idx >= 0 ){
             if(validate_size(size, &(programs[command_idx]))) {
-				sys_create_child_process(parameters, programs[command_idx].base_priority, STDIN, STDOUT, (uint64_t) programs[command_idx].function_ptr);
+				char ** params = prepare_parameters(parameters, size, command);
+				sys_create_child_process(params, programs[command_idx].base_priority, STDIN, STDOUT, (uint64_t) programs[command_idx].function_ptr);
 				sys_wait_for_children();
 			}
 		}
